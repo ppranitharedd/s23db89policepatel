df6c60e4693f1ae6bc7c7996462c6e49c27e3e79
ppranitharedd <policepatelpranitha03@gmail.com>
2023-11-08 11:51:06 -0600

    updated the name

--- controllers/api.js
+++ controllers/api.js
@@ -1,6 +1,6 @@
 exports.api = function(req, res) {
     res.write('[');
-    res.write('{"resource":"costumes", ');
+    res.write('{"resource":"TastyFood", ');
     res.write(' "verbs":["GET","PUT", "DELETE"] ');
     res.write('}');
     res.write(']')

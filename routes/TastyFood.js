011571d9ba880402899099d60cb9e084a1b1c46b
ppranitharedd <policepatelpranitha03@gmail.com>
2023-11-08 12:04:57 -0600

    Update TastyFood.js

--- routes/TastyFood.js
+++ routes/TastyFood.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var express = require('express');
+const tastyFood_controlers= require('../controllers/TastyFood');
+var router = express.Router();
+/* GET tastyFoods */
+router.get('/', tastyFood_controlers.tastyFood_view_all_Page );
+module.exports = router;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('TastyFood', { title: 'Search Results Tasty Foods' });
